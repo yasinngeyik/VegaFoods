fce2532f7e7f0ebac120aa896fefed17a267074a
yasinngeyik <yasinngeyik@gmail.com>
2025-04-08 00:26:18 +0300

    add new code

--- app.js
+++ app.js
@@ -1,3 +1,45 @@
+async function loadProducts() {
+    try {
+        const { products } = await (await fetch("/product.json")).json();
+        const productsContainer = document.querySelector("#products-container");
+
+        const productsToShow = products.slice(0, products.length - 7); //bununla arkadakileri sildim
+
+        productsToShow.forEach(product => {
+            const productDiv = document.createElement("div");
+            productDiv.classList.add("product-card");
+
+            productDiv.innerHTML = `
+                <img src="${product.image}" alt="${product.name}" class="product-image rounded-t-lg">
+                <div class="icons-container">
+                    <button class="icon-button">
+                        <i class="fa-solid fa-bars"></i>
+                    </button>
+                    <button class="icon-button">
+                        <i class="fa-solid fa-cart-shopping"></i>
+                    </button>
+                    <button class="icon-button">
+                        <i class="fa-solid fa-heart"></i>
+                    </button>
+                </div>
+                <div class="product-info">
+                    <h3 class="product-name">${product.name}</h3>
+                    <p class="product-price">$${product.discountedPrice || product.price}</p>
+                    ${product.discountedPrice ? `<p class="old-price">$${product.price}</p>` : ''}
+                </div>
+            `;
+
+            productsContainer.appendChild(productDiv);
+        });
+    } catch (error) {
+        console.error("Ürünler yüklenirken hata oluştu:", error);
+    }
+}
+
+loadProducts();
+
+
+
 // navbar-toggle
 const menuToggle = document.getElementById('menu-toggle');
 const mobileMenu = document.getElementById('mobile-menu');
